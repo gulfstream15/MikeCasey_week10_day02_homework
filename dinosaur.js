61822b4f15b248231dc691eef571d4834e000a35
User <user@Users-MBP.home>
2017-06-13 20:57:48 +0100

    second test dinosaur has offspring per year passes

--- dinosaur.js
+++ dinosaur.js
@@ -1,6 +1,6 @@
 var Dinosaur = function (options) {
   this.type = options.type;
-  this.offspring = options.offspring;
+  this.offspring_per_year= options.offspring_per_year;
 }
 
 module.exports = Dinosaur;
--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -6,11 +6,15 @@ describe('Dinosaur', function () {
   var dinosaur;
 
   beforeEach(function () {
-    dinosaur = new Dinosaur({ type: 'Tyrannosaurus', offspring: 3 });
+    dinosaur = new Dinosaur({ type: 'Tyrannosaurus', offspring_per_year: 3 });
   });
 
   it('should have a type', function () {
     assert.strictEqual('Tyrannosaurus', dinosaur.type);
+  });
+
+  it('should have offspring per year', function () {
+    assert.strictEqual(3, dinosaur.offspring_per_year);
   });
 
 });
